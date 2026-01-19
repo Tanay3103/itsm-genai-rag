25732505ffd882a4ba7f9f736b2a61ea16e7bed0
Tanay Chaudhary <88741982+Tanay3103@users.noreply.github.com>
2026-01-19 11:59:11 +0530

    Refactor ingest function and update imports

--- utils/auto_ingest.py
+++ utils/auto_ingest.py
@@ -1,12 +1,12 @@
-from langchain.vectorstores import Qdrant
 from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Qdrant
 from utils.qdrant_client import get_qdrant_client
 from config import TICKET_COLLECTION
 
-def ingest_resolved_ticket(text, metadata):
+def ingest_ticket(text, metadata):
     Qdrant.from_texts(
-        [text],
-        OpenAIEmbeddings(),
+        texts=[text],
+        embedding=OpenAIEmbeddings(),
         metadatas=[metadata],
         client=get_qdrant_client(),
         collection_name=TICKET_COLLECTION
